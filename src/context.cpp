da09ccfce4c8e61502fc36ba6598da528f9ef940
Robey Pointer <robeypointer@gmail.com>
2012-09-09 13:04:11 -0400

    checkpoint crazytown before i erase the old code.

--- src/context.cpp
+++ src/context.cpp
@@ -4,33 +4,20 @@
 
 using namespace v8;
 
-Persistent<FunctionTemplate> NodeLContext::constructor;
-
-inline Persistent<FunctionTemplate> makeConstructor(const char *name) {
-  Local<FunctionTemplate> f = FunctionTemplate::New();
-  Persistent<FunctionTemplate> proto = Persistent<FunctionTemplate>::New(f);
-  proto->SetClassName(String::NewSymbol(name));
-  proto->InstanceTemplate()->SetInternalFieldCount(1);
-  return proto;
-}
-
-inline void addMethod(Handle<FunctionTemplate> ctor, const char *name, InvocationCallback cb) {
-  Local<FunctionTemplate> f = FunctionTemplate::New(cb);
-  ctor->InstanceTemplate()->Set(String::NewSymbol(name), f);
-}
+NodePrototype<NodeLContext> NodeLContext::proto("LLVMContext");
 
 void NodeLContext::init() {
   HandleScope scope;
 
-  constructor = makeConstructor("LLVMContext");
-  addMethod(constructor, "newModule", newModule);
-  constructor->InstanceTemplate()->Set(String::NewSymbol("msg"), String::New("Okay, that worked."));
+  proto.addMethod("newModule", newModule);
+  proto.addMethod__("newModule__", &NodeLContext::newModule__);
+  proto.addField("msg", String::New("Okay, that worked."));
 }
 
 Handle<Value> NodeLContext::create(llvm::LLVMContext& c) {
   HandleScope scope;
 
-  Local<Object> jsobj = constructor->GetFunction()->NewInstance();
+  Local<Object> jsobj = proto.create();
   (new NodeLContext(c))->Wrap(jsobj);
   return scope.Close(jsobj);
 }
@@ -40,6 +27,13 @@ Handle<Value> NodeLContext::newModule(const Arguments& args) {
   NodeLContext *self = Unwrap<NodeLContext>(args.This());
   printf("id=%d\n", self->id);
   Local<String> rv = String::New("hello!");
+  return scope.Close(rv);
+}
+
+Handle<Value> NodeLContext::newModule__(const Arguments& args) {
+  HandleScope scope;
+  printf("id=%d\n", id);
+  Local<String> rv = String::New("hello from crazy town!");
   return scope.Close(rv);
 }
 
--- src/context.h
+++ src/context.h
@@ -6,8 +6,24 @@
 
 using namespace v8;
 
+/*
+ * somewhat ridiculous: C++ doesn't allow casting method pointers to/from
+ * (void *) because it allows compilers to make method pointers be larger
+ * than a pointer!
+ */
+template <typename T>
+class Suitcase {
+public:
+  T item;
 
+  Suitcase(T x) : item(x) { }
+};
+
+template <typename T>
 class NodePrototype {
+public:
+  typedef Handle<Value> (T::*Method)(const Arguments& args);
+
 private:
   Persistent<FunctionTemplate> constructor;
 
@@ -24,6 +40,24 @@ public:
     constructor->InstanceTemplate()->Set(String::NewSymbol(name), f);
   }
 
+  void addMethod__(const char *name, Method method) {
+    // in theory, this is a leak, but it's only done at startup, and needed forever.
+    Suitcase<Method> *suitcase = new Suitcase<Method>(method);
+    Local<FunctionTemplate> f = FunctionTemplate::New(trampoline, External::Wrap(suitcase));
+    constructor->InstanceTemplate()->Set(String::NewSymbol(name), f);
+  }
+
+  static Handle<Value> trampoline(const Arguments& args) {
+    Method method = reinterpret_cast<Suitcase<Method> *>(External::Unwrap(args.Data()))->item;
+    // this is a bit evil. manual implementation of node's Unwrap:
+    T *self = static_cast<T *>(args.This()->GetPointerFromInternalField(0));
+    return (self->*method)(args);
+  }
+
+  void addField(const char *name, Handle<Value> value) {
+    constructor->InstanceTemplate()->Set(String::NewSymbol(name), value);
+  }
+
   Local<Object> create() {
     return constructor->GetFunction()->NewInstance();
   }
@@ -35,7 +69,7 @@ private:
   llvm::LLVMContext& llvmContext;
   int id;
 
-  static Persistent<FunctionTemplate> constructor;
+  static NodePrototype<NodeLContext> proto;
 
 public:
   NodeLContext(llvm::LLVMContext& c) : llvmContext(c), id(23) { }
@@ -43,4 +77,5 @@ public:
   static void init();
   static Handle<Value> create(llvm::LLVMContext& c);
   static Handle<Value> newModule(const Arguments& args);
+  Handle<Value> newModule__(const Arguments& args);
 };
