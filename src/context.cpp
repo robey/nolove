d8a19f99ebf76c1b89eebb8ea8a8727ab6193624
Robey Pointer <robeypointer@gmail.com>
2012-09-09 13:13:09 -0400

    finish cleanup and always use method pointers.

--- src/context.cpp
+++ src/context.cpp
@@ -4,13 +4,12 @@
 
 using namespace v8;
 
-NodePrototype<NodeLContext> NodeLContext::proto("LLVMContext");
+NodeProto<NodeLContext> NodeLContext::proto("LLVMContext");
 
 void NodeLContext::init() {
   HandleScope scope;
 
-  proto.addMethod("newModule", newModule);
-  proto.addMethod__("newModule__", &NodeLContext::newModule__);
+  proto.addMethod("newModule", &NodeLContext::newModule);
   proto.addField("msg", String::New("Okay, that worked."));
 }
 
@@ -24,18 +23,7 @@ Handle<Value> NodeLContext::create(llvm::LLVMContext& c) {
 
 Handle<Value> NodeLContext::newModule(const Arguments& args) {
   HandleScope scope;
-  NodeLContext *self = Unwrap<NodeLContext>(args.This());
-  printf("id=%d\n", self->id);
-  Local<String> rv = String::New("hello!");
-  return scope.Close(rv);
-}
-
-Handle<Value> NodeLContext::newModule__(const Arguments& args) {
-  HandleScope scope;
   printf("id=%d\n", id);
   Local<String> rv = String::New("hello from crazy town!");
   return scope.Close(rv);
 }
-
-//  v8::Local<v8::FunctionTemplate> templ = v8::FunctionTemplate::New(callback);
- //   target->PrototypeTemplate()->Set(v8::String::NewSymbol(name), templ);
---
+++ src/nodeproto.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <v8.h>
+#include <node.h>
+#include "llvm/LLVMContext.h"
+
+using namespace v8;
+
+/*
+ * somewhat ridiculous: C++ doesn't allow casting method pointers to/from
+ * (void *) because it allows compilers to make method pointers be larger
+ * than a pointer!
+ */
+template <typename T>
+class Suitcase {
+public:
+  T item;
+
+  Suitcase(T x) : item(x) { }
+};
+
+template <typename T>
+class NodeProto {
+public:
+  typedef Handle<Value> (T::*Method)(const Arguments& args);
+
+private:
+  Persistent<FunctionTemplate> constructor;
+
+public:
+  NodeProto(const char *name) {
+    Local<FunctionTemplate> f = FunctionTemplate::New();
+    constructor = Persistent<FunctionTemplate>::New(f);
+    constructor->SetClassName(String::NewSymbol(name));
+    constructor->InstanceTemplate()->SetInternalFieldCount(1);
+  }
+
+  void addMethod(const char *name, Method method) {
+    // in theory, this is a leak, but it's only done at startup, and needed forever.
+    Suitcase<Method> *suitcase = new Suitcase<Method>(method);
+    Local<FunctionTemplate> f = FunctionTemplate::New(trampoline, External::Wrap(suitcase));
+    constructor->InstanceTemplate()->Set(String::NewSymbol(name), f);
+  }
+
+  static Handle<Value> trampoline(const Arguments& args) {
+    Method method = reinterpret_cast<Suitcase<Method> *>(External::Unwrap(args.Data()))->item;
+    // this is a bit evil. manual implementation of node's Unwrap:
+    T *self = static_cast<T *>(args.This()->GetPointerFromInternalField(0));
+    return (self->*method)(args);
+  }
+
+  void addField(const char *name, Handle<Value> value) {
+    constructor->InstanceTemplate()->Set(String::NewSymbol(name), value);
+  }
+
+  Local<Object> create() {
+    return constructor->GetFunction()->NewInstance();
+  }
+};
